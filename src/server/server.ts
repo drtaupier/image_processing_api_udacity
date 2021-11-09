c941e2d8081aebea9e73a9980cc72f1eeac3ca67
David Rivera <drtaupier@gmail.com>
2021-11-08 22:36:43 -0500

    I configured public static file

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 import routes from './routes/index';
+import path, { dirname } from 'path';
+import morgan from 'morgan';
+
 const app = express();
 const port = 3000;
 
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.static('dist'));
+app.use('/public', express.static('public'));
 app.use('/api', routes);
+
+app.set('views', path.resolve(__dirname, '../../src/client/views'));
 
 app.listen(port, () => {
 	console.log(`Server started at localhost ${port}`);
