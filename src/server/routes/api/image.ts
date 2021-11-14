9b4c74cbaf46a352317d7680bcc38220d195adb2
David Rivera <drtaupier@gmail.com>
2021-11-14 14:44:40 -0500

    I added the server response and added if the file exits or no in our output directory

--- src/server/routes/api/image.ts
+++ src/server/routes/api/image.ts
@@ -1,6 +1,8 @@
 import sharp from 'sharp';
 import express from 'express';
 import imgValidations from '../../middleware/errors';
+import path from 'path';
+import fs from 'fs';
 const images = express.Router();
 
 images.get('/', async (req, res) => {
@@ -11,22 +13,32 @@ images.get('/', async (req, res) => {
 	const filenameImg: string = filename.toString();
 	const widthImg: number = parseInt(width, 10);
 	const heightImg: number = parseInt(height, 10);
+	const directory = './public/output';
+	const files = fs.readdirSync(directory);
+	const imgComplete: string = `${filename}${width}x${height}.jpg`.toString();
 	try {
 		imgValidations(query);
-		const newFile = `./public/output/${filenameImg}${widthImg}x${heightImg}.jpg`;
-		await sharp(`./public/images/${filenameImg}.jpg`)
-			.resize(widthImg, heightImg, {
-				fit: 'contain',
-				background: {
-					r: 0,
-					b: 0,
-					g: 0,
-				},
-			})
-			.toFile(newFile);
+		const newFile = `./public/output/${imgComplete}`;
 		const newFileUrl: string = newFile.slice(1); //Quitamos el punto con el que inicia el string
-		console.log(newFileUrl);
-		res.redirect(`http://localhost:3000${newFileUrl}`);
+		const url = path.join(__dirname, `../../../..${newFileUrl}`);
+		if (files.includes(imgComplete) === true) {
+			console.log(files.indexOf(imgComplete));
+			console.log('Archivos: ', files);
+			console.log('File enviado: ', imgComplete);
+			res.status(200).sendFile(url);
+		} else {
+			await sharp(`./public/images/${filenameImg}.jpg`)
+				.resize(widthImg, heightImg, {
+					fit: 'contain',
+					background: {
+						r: 0,
+						b: 0,
+						g: 0,
+					},
+				})
+				.toFile(newFile);
+			res.status(201).sendFile(url);
+		}
 	} catch (error: any) {
 		res.status(400).json({
 			status: 'error',
