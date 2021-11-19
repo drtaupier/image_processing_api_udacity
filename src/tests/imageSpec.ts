fad91dcbaafa878bc4c0e7cdcd0705644896b3ae
David Rivera <drtaupier@gmail.com>
2021-11-19 01:44:29 -0500

    I added a new test to imageResize function

--- src/tests/imageSpec.ts
+++ src/tests/imageSpec.ts
@@ -1,6 +1,7 @@
 import images from '../server/routes/api/image';
 import routes from '../server/routes/index';
 import request from 'supertest';
+import imageResize from '../server/middleware/utilities';
 
 describe('GET /api', function () {
 	it('respond with json format the next steps', function () {
@@ -17,5 +18,12 @@ describe('GET /api/images/:filename/:width/:height', () => {
 		request(images)
 			.get('/images/filename=santamonica&width=400&height=200')
 			.expect(200);
+	});
+});
+
+describe('Function imageResize', () => {
+	it('Should return expected result for a string parameter and two numbers parameters', () => {
+		const result = imageResize('santamonica', 800, 400);
+		expect(result).toBeTrue;
 	});
 });
